1139abcd193a789f67b14c2a6fe384866a4e8ab7
Dan Puttick <dputtick@users.noreply.github.com>
2017-06-15 16:57:44 -0400

    Put config in separate file

--- app.py
+++ app.py
@@ -9,19 +9,6 @@ class Root():
             return message
 
 
-def config():
-    conf = {
-        'global': {
-            'server.socket_host': '127.0.0.1',
-            'server.socket_port': 8000,
-            # 'environment': production,
-            'log.access_file': '',
-            'log.error_file': ''
-        }
-    }
-    return conf
-
-
 if __name__ == '__main__':
-    conf = config()
-    cherrypy.quickstart(Root(), '/', conf)
+    cherrypy.config.update("global.cfg")
+    cherrypy.quickstart(Root(), '/')
