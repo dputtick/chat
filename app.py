40fe58ed62202554b46425c901cd4cdace742ead
Dan Puttick <dputtick@users.noreply.github.com>
2017-06-19 13:49:23 -0400

    Add sessions to app.py

--- app.py
+++ app.py
@@ -6,7 +6,16 @@ class Root():
     @cherrypy.expose
     def index(self, message=None):
         if message:
-            return message
+            if not cherrypy.session.get('messages'):
+                cherrypy.session['messages'] = []
+            cherrypy.session['messages'].append(message)
+        messages = cherrypy.session['messages']
+        retlist = [m + '\n' for m in messages[:-1]]
+        retlist.append(messages[-1])
+        return ''.join(retlist)
+
+    def add_message(message):
+        pass
 
 
 if __name__ == '__main__':
