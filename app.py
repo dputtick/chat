e3eb9ec6d3dcee41982da17358c33bb932c71ccf
Dan Puttick <dputtick@users.noreply.github.com>
2017-07-28 16:12:21 -0400

    REST endpoint works

--- app.py
+++ app.py
@@ -1,21 +1,49 @@
 import cherrypy
+import psycopg2
 
 
 class Root():
 
     @cherrypy.expose
-    def index(self, message=None):
-        if message:
-            self.add_message(message)
-        messages = cherrypy.session['messages']
-        return ' '.join(messages)
+    def index(self):
+        return 'index'
 
-    def add_message(self, message):
-        if not cherrypy.session.get('messages'):
+
+@cherrypy.expose
+class Messages():
+
+    def GET(self):
+        if cherrypy.session.get('messages', None):
+            messages = ' '.join(cherrypy.session['messages'])
+        else:
+            messages = 'No messages'
+        return messages
+
+    def POST(self, message=None):
+        if not cherrypy.session.get('messages', None):
             cherrypy.session['messages'] = []
         cherrypy.session['messages'].append(message)
 
+    def add_message(self, mess_id, message):
+        dbname = cherrypy.config['database']['dbname']
+        username = cherrypy.config['database']['user']
+        conn = psycopg2.connect(dbname + ' ' + username)
+        cursor = conn.cursor()
+
+    def get_message(self, mess_id):
+        dbname = cherrypy.config['database']['dbname']
+        username = cherrypy.config['database']['user']
+        conn = psycopg2.connect(dbname + ' ' + username)
+        cursor = conn.cursor()
+
+
+def run():
+    cherrypy.config.update('server.cfg')
+    cherrypy.tree.mount(Root(), '/')
+    cherrypy.tree.mount(Messages(), '/messages', 'app.cfg')
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+
 
 if __name__ == '__main__':
-    cherrypy.config.update('global.cfg')
-    cherrypy.quickstart(Root(), '/', 'global.cfg')
+    run()
